2c932e85c8cb181e846a7fe2dd314ff9183d0259
Karcaglior <antoniomornmuoz@gmail.com>
2017-11-14 09:33:43 +0100

    Action settings added

--- app/src/main/java/com/example/antoniomoranmunoz/filmlibrary/MainActivity.java
+++ app/src/main/java/com/example/antoniomoranmunoz/filmlibrary/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.antoniomoranmunoz.filmlibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
