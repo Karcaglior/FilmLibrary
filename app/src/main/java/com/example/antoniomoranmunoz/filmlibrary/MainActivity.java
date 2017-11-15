37d18e521b5a964b7f2394f882e1480191e2cb90
Karcaglior <antoniomornmuoz@gmail.com>
2017-11-15 13:57:54 +0100

    Added onClickListeners in Main Activity

--- app/src/main/java/com/example/antoniomoranmunoz/filmlibrary/MainActivity.java
+++ app/src/main/java/com/example/antoniomoranmunoz/filmlibrary/MainActivity.java
@@ -6,8 +6,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String SECTION_LIBRARY = "Library";
+    public static final String SECTION_CINEMA = "Cinema";
+    public static final String SECTION_PEOPLE = "People";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+        RelativeLayout libraryBtn = (RelativeLayout)findViewById(R.id.libraryBtn);
+        RelativeLayout cinemaBtn = (RelativeLayout)findViewById(R.id.cinemaBtn);
+        RelativeLayout peopleBtn = (RelativeLayout)findViewById(R.id.peopleBtn);
+
+        libraryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailSection(MainActivity.SECTION_LIBRARY);
+            }
+        });
+
+        cinemaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailSection(MainActivity.SECTION_CINEMA);
+            }
+        });
+
+        peopleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailSection(MainActivity.SECTION_PEOPLE);
+            }
+        });
+    }
+
+    public void loadDetailSection(String sectionTitle) {
+
     }
 
     @Override
