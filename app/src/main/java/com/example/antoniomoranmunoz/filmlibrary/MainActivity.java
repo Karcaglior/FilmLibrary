1ae7a2970dfb84e0330786597688cf456c2abc15
Karcaglior <antoniomornmuoz@gmail.com>
2017-11-19 19:34:36 +0100

    Added change brightness functionality

--- app/src/main/java/com/example/antoniomoranmunoz/filmlibrary/MainActivity.java
+++ app/src/main/java/com/example/antoniomoranmunoz/filmlibrary/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.antoniomoranmunoz.filmlibrary;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,7 +15,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.antoniomoranmunoz.filmlibrary.Helper.LocaleHelper;
 
@@ -30,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
     TextView cinemaText;
     TextView peopleText;
 
+    SeekBar seekBar;
+    boolean success = false;
+
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(LocaleHelper.onAttach(newBase, "en"));
@@ -37,10 +46,43 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+        seekBar.setMax(255);
+        seekBar.setProgress(getBrightness());
+
+        getPermission();
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                if(fromUser && success) {
+                    setBrightness(progress);
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                if(!success) {
+                    Toast.makeText(MainActivity.this, "Permission not granted!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
         RelativeLayout libraryBtn = (RelativeLayout)findViewById(R.id.libraryBtn);
         RelativeLayout cinemaBtn = (RelativeLayout)findViewById(R.id.cinemaBtn);
@@ -81,6 +123,58 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void setBrightness(int brigthness) {
+        if(brigthness<0) {
+            brigthness = 0;
+        } else if(brigthness > 255) {
+            brigthness = 255;
+        }
+
+        ContentResolver contentResolver = getApplicationContext().getContentResolver();
+        Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, brigthness);
+    }
+
+    private int getBrightness() {
+        int brightness = 100;
+        try{
+            ContentResolver contentResolver = getApplicationContext().getContentResolver();
+            brightness = Settings.System.getInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS);
+        }catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return brightness;
+    }
+
+    private void getPermission() {
+        boolean value;
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            value = Settings.System.canWrite(getApplicationContext());
+            if(value) {
+                success = true;
+            } else {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+                startActivityForResult(intent, 1000);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 1000) {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                boolean value = Settings.System.canWrite(getApplicationContext());
+                if(value) {
+                    success = true;
+                } else {
+                    Toast.makeText(this, "Permission not granted!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
     private void updateView(String lang) {
         Context context = LocaleHelper.setLocale(this, lang);
 
@@ -114,6 +208,9 @@ public class MainActivity extends AppCompatActivity {
             updateView((String)Paper.book().read("language"));
         } else if(item.getItemId() == R.id.action_settings) {
             Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        } else if(item.getItemId() == R.id.action_theme) {
+            Intent intent = new Intent(MainActivity.this, ThemeActivity.class);
             startActivity(intent);
         }
 
--- app/src/main/java/com/example/antoniomoranmunoz/filmlibrary/ThemeActivity.java
+++ app/src/main/java/com/example/antoniomoranmunoz/filmlibrary/ThemeActivity.java
@@ -1,13 +1,50 @@
 package com.example.antoniomoranmunoz.filmlibrary;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toolbar;
 
 public class ThemeActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme);
+
+        RelativeLayout sunLayout = (RelativeLayout)findViewById(R.id.sunLayout);
+        RelativeLayout moonLayout = (RelativeLayout)findViewById(R.id.moonLayout);
+        final ConstraintLayout constraintTheme = (ConstraintLayout)findViewById(R.id.constraintTheme);
+
+        sunLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTheme(R.style.SunTheme);
+                constraintTheme.setBackgroundColor(Color.parseColor("#000000"));
+                setContentView(R.layout.activity_theme);
+                finish();
+
+            }
+        });
+
+        moonLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTheme(R.style.MoonTheme);
+                constraintTheme.setBackgroundColor(Color.parseColor("#ffffff"));
+                setContentView(R.layout.activity_theme);
+                finish();
+
+            }
+        });
+
     }
+
+
 }
